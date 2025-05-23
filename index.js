5e5734f255265ac2f2d476220bafd95a0cc1b6b3
José Guilherme <gt47512@gmail.com>
2025-05-23 17:01:30 -0300

    Realizado a rota do login e o controller do mesmo, juntamente com o tokken e diferenciação de usuário e admin

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./src/routes/userRoutes.js";
 import adminRoutes from "./src/routes/adminRoutes.js";
+import loginRoutes from "./src/routes/loginRouters.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api", loginRoutes);
 app.use("/api", userRoutes);
 app.use("/api", adminRoutes);
 
